ce6ec3d84c505726ab18b432a04c86d880e792ba
Ryan Blair <rblair@gmail.com>
2014-01-04 08:13:11 +0000

    initial pass at jruby

---
+++ jruby/build.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+# Portions Copyright 2014 Ryan Blair
+#
+# Load support functions
+. ../../lib/functions.sh
+. ../../../lib/more-functions.sh #from epoxy-ips-bootstrap
+
+PROG=jruby    # App name
+VER=1.7.9     # App version
+VERHUMAN=$VER # Human-readable version
+#PVER=         # Branch (set in config.sh, override here if needed)
+PKG=language/runtime/jruby # Package name (e.g. library/foo)
+SUMMARY="Ruby programming language for the JVM" # One-liner, must be filled in
+DESC="JRuby is a high performance, stable, fully threaded Java \
+    implementation of the Ruby programming language"
+
+# reconfigure download to use a global site
+MIRROR=jruby.org.s3.amazonaws.com
+
+build_jruby() {
+    logmsg "Installing JRuby bits from distribution bin tarball"
+    mkdir -p $DESTDIR/$PREFIX/$PROG/$VER || \
+        logerr "--- Failed to create installation directory"
+    pushd $TMPDIR/$BUILDDIR > /dev/null
+    logcmd cp -r bin docs lib samples tool $DESTDIR/$PREFIX/$PROG/$VER || \
+        logerr "--- Failed to copy JRuby bits into DESTDIR"
+    popd > /dev/null
+}
+
+init
+download_source downloads/$VER ${PROG}-bin $VER
+prep_build
+build_jruby
+generate_localmog
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
