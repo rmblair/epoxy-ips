4f7a0281d5267eb1871e81412817100d59ef9806
Ryan Blair <rblair@gmail.com>
2014-01-11 10:16:11 -0800

    upgrade to ruby 2.0

--- puppet/vars.sh
+++ puppet/vars.sh
@@ -2,9 +2,15 @@ AUGEAS_VER=1.1.0
 FACTER_VER=1.7.4
 FFI_VER=3.0.13
 HIERA_VER=1.3.0
-RUBY_VER=1.9.3-p484
 RUBY_AUGEAS_VER=0.5.0
 YAML_VER=0.1.4
+
+#RUBY_VER=1.9.3-p484
+# this is what ruby uses in its gem path, despite being != current (1.9.3?)
+#RUBY_LIB_VER=1.9.1
+
+RUBY_VER=2.0.0-p353
+RUBY_LIB_VER=2.0.0
 
 # maybe something in 3.4 breaks for you?
 #PUPPET_VER=3.3.2
