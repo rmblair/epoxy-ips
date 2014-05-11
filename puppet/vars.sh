5a90260645bcfd6f6200b6b6c6a2526cbc3f2c60
Ryan Blair <rblair@gmail.com>
2014-05-11 19:16:29 +0000

    updated to puppet 3.5.1 and latest deps

--- puppet/vars.sh
+++ puppet/vars.sh
@@ -1,18 +1,19 @@
-AUGEAS_VER=1.1.0
-FACTER_VER=1.7.4
+AUGEAS_VER=1.2.0
+FACTER_VER=1.7.5
 FFI_VER=3.0.13
-HIERA_VER=1.3.1
+HIERA_VER=1.3.2
 RUBY_AUGEAS_VER=0.5.0
-YAML_VER=0.1.4
+YAML_VER=0.1.5
 
 #RUBY_VER=1.9.3-p484
 # this is what ruby uses in its gem path, despite being != current (1.9.3?)
 #RUBY_LIB_VER=1.9.1
 
-RUBY_VER=2.0.0-p353
+RUBY_VER=2.0.0-p481
 RUBY_LIB_VER=2.0.0
 
 # maybe something in 3.4 breaks for you?
 #PUPPET_VER=3.2.4
 #PUPPET_VER=3.3.2
-PUPPET_VER=3.4.3
+#PUPPET_VER=3.4.3
+PUPPET_VER=3.5.1
