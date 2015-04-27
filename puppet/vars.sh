63f41e8775e64a71b1ffc252fe9a110caccdb36a
Ryan Blair <rblair@gmail.com>
2015-04-27 00:56:04 +0000

    update puppet to 3.7.5 along with ruby to 2.1.6

--- puppet/vars.sh
+++ puppet/vars.sh
@@ -9,11 +9,19 @@ YAML_VER=0.1.5
 # this is what ruby uses in its gem path, despite being != current (1.9.3?)
 #RUBY_LIB_VER=1.9.1
 
-RUBY_VER=2.0.0-p645
-RUBY_LIB_VER=2.0.0
+#RUBY_VER=2.0.0-p645
+#RUBY_LIB_VER=2.0.0
+
+RUBY_VER=2.1.6
+RUBY_LIB_VER=2.1.0
+
+# not safe w/ 3.7.5 ... yet
+#RUBY_VER=2.2.2
+#RUBY_LIB_VER=2.2.0
 
 # maybe something in 3.4 breaks for you?
 #PUPPET_VER=3.2.4
 #PUPPET_VER=3.3.2
 #PUPPET_VER=3.4.3
-PUPPET_VER=3.5.1
+#PUPPET_VER=3.5.1
+PUPPET_VER=3.7.5
